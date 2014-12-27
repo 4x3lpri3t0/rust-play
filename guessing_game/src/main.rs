4da2ef444a9eb64989c56d8f1c5aea5975d44b1f
Zedronar <prietoaxel@gmail.com>
2014-12-27 14:36:05 -0300

    guessing game finished

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,43 @@
+use std::io;
+use std::rand;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = (rand::random::<uint>() % 100u) + 1u; // secret_number: u
+
+    loop {
+        println!("Please input your guess.");
+
+        let input = io::stdin().read_line()
+                               .ok()
+                               .expect("Failed to read line");
+        let input_num: Option<uint> = input.trim().parse();
+        // parse() sees the string "5\n", so with trim() we delete whitespaces
+
+        let num = match input_num {
+            Some(num) => num,
+            None      => {
+                println!("Please input a member!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", num);
+
+        match cmp(num, secret_number) {
+            Less    => println!("Too small!"),
+            Greater => println!("Too big!"),
+            Equal    => {
+                println!("You win!");
+                return;
+            },
+        }
+    };
+}
+
+fn cmp(a: uint, b: uint) -> Ordering {
+    if a < b { Less }
+    else if a > b { Greater }
+    else { Equal }
 }
